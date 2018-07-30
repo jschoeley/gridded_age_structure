c5411bc75a6fb3f6d3c7d80f47998f6afe5ae95d
Jonas Schöley <jschoeley@gmail.com>
2018-07-30 12:14:07 +0200

    Add Different Spatial Resolutions and Hex

--- 2018-07-27-gridded_age_structure.R
+++ 2018-07-27-gridded_age_structure.R
@@ -35,12 +35,34 @@ eura <-
   fortify() %>%
   rename(longitude = long, latitude = lat)
 
-# divide the european continent into a 100 by 100 km grid
-euro_grid <-
-  st_make_grid(euro_nuts3_sf, crs = 3035, what = 'polygons', cellsize = 100*1e3)
+# divide the european continent into various grids
+# 100 by 100 km rectangular grid
+euro_grid_rect_100x100km <-
+  st_make_grid(euro_nuts3_sf, crs = 3035, what = 'polygons', cellsize = 100e3)
+# 50 by 50 km rectangular grid
+euro_grid_rect_50x50km <-
+  st_make_grid(euro_nuts3_sf, crs = 3035, what = 'polygons', cellsize = 50e3)
+# 25 by 25 km rectangular grid
+euro_grid_rect_25x25km <-
+  st_make_grid(euro_nuts3_sf, crs = 3035, what = 'polygons', cellsize = 25e3)
+# 10,000 km2 hex grid
+# hexagons parametrized by minimal diameter (d) in st_make_grid() function
+# therefore if area A is given:
+# d = sqrt(2)*sqrt(A)/3^(1/4)
+euro_grid_hex_10000km2 <-
+  st_make_grid(euro_nuts3_sf, crs = 3035, what = 'polygons',
+               cellsize = sqrt(2)*sqrt(100e3^2)/3^(1/4), square = FALSE)
+# 2500 km2 hex grid
+euro_grid_hex_2500km2 <-
+  st_make_grid(euro_nuts3_sf, crs = 3035, what = 'polygons',
+               cellsize = sqrt(2)*sqrt(50e3^2)/3^(1/4), square = FALSE)
 
-# SANITY CHECK: are the grid cell areas identical
-range(st_area(euro_grid))
+# SANITY CHECK: are the grid cell areas identical?
+range(st_area(euro_grid_rect_100x100km))
+range(st_area(euro_grid_rect_50x50km))
+range(st_area(euro_grid_rect_25x25km))
+range(st_area(euro_grid_hex_10000km2))
+range(st_area(euro_grid_hex_2500km2))
 
 # interpolate population counts by age over the grid
 # and extract grid centroids
@@ -49,10 +71,25 @@ euro_grid_age_sf <-
   gather(key, value, total, age65plus, age0to15, age15to65) %>%
   group_by(key) %>%
   do(
-  # area weighted grid interpolation preserving totals
-    select(., value) %>%
-      st_interpolate_aw(to = euro_grid, extensive = TRUE) %>%
-      st_centroid()
+    # area weighted grid interpolation preserving totals
+    bind_rows(
+      rect_100x100km = select(., value) %>%
+        st_interpolate_aw(to = euro_grid_rect_100x100km, extensive = TRUE) %>%
+        st_centroid(),
+      rect_50x50km = select(., value) %>%
+        st_interpolate_aw(to = euro_grid_rect_50x50km, extensive = TRUE) %>%
+        st_centroid(),
+      rect_25x25km = select(., value) %>%
+        st_interpolate_aw(to = euro_grid_rect_25x25km, extensive = TRUE) %>%
+        st_centroid(),
+      hex_10000km2 = select(., value) %>%
+        st_interpolate_aw(to = euro_grid_hex_10000km2, extensive = TRUE) %>%
+        st_centroid(),
+      hex_2500km2 = select(., value) %>%
+        st_interpolate_aw(to = euro_grid_hex_2500km2, extensive = TRUE) %>%
+        st_centroid(),
+      .id = 'grid_type'
+    )
   ) %>%
   # projection flag got lost, reset it
   st_as_sf(crs = 3035) %>%
@@ -93,7 +130,7 @@ euro_grid_age_df <-
   as_data_frame() %>%
   select(-geometry) %>%
   rename(grid_id = Group.1, longitude = X, latitude = Y) %>%
-  arrange(desc(total))
+  arrange(grid_type, desc(total))
 
 # SANITY CHECK: do the differences make sense?
 # should be approx 0
@@ -113,18 +150,17 @@ euro_grid_age_df$col <-
                        contrast = .5, lightness = 1, chroma = 1, hue = 2/12)
 
 # bubble-grid-map
-euro_grid_age_df %>%
+euro_grid_age_df %>% filter(grid_type == 'hex_2500km2') %>%
   ggplot(aes(x = longitude, y = latitude)) +
   geom_polygon(aes(group = group), data = eura, color = 'white', fill = 'grey95') +
   geom_point(aes(col = col, size = total), show.legend = FALSE) +
   coord_map(xlim = c(-30, 50), ylim = c(35, 70), projection = 'lambert', 43, 62) +
   scale_size_area(
     'Population size',
-    max_size = 8,
+    max_size = 3,
     breaks = c(1e3, 1e4, 1e5, 5e5),
     labels = c('1,000', '10,000', '100,000', '500,000')) +
   scale_color_identity() +
   theme_void() +
-  theme(legend.position = c(0.83, 0.7)) +
   labs(caption = 'Data: Eurostat')
 
